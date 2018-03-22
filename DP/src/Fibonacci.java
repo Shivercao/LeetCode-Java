d8212af903754093a87e26cd3e138a64990d3673
Shivercao <zhc53@pitt.edu>
2018-03-22 01:21:04 -0400

    add Look_and_Say leetcode

---
+++ DP/src/Fibonacci.java
@@ -0,0 +1,23 @@
+/**
+ * Created by shivercao on 3/20/2018.
+ */
+public class Fibonacci {
+    final int Max = 1000;
+    final int Min = -1;
+    int[] look = new int[Max];
+
+    void initialized(){
+        for(int i = 0; i < Max; i++ ){
+            look[i] = Min;
+        }
+    }
+    public int fib_Memo(int n){
+        if(look[n] == Min){
+            if(n <= 1) return n;
+            else{
+                look[n] = fib_Memo(n-1) + fib_Memo(n-2);
+            }
+        }
+        return look[n];
+    }
+}
